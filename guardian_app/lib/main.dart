2d1f42e10d699838213753a8ba3967f97a3a94d2
asmaa1596 <asmaa.elzayyadi@gmail.com>
2021-01-21 01:34:51 +0800

    bottom nav bar stays across home and profile pages. need to change the size of the actual nav bar because its too tall. need to change the sizes of the icons and text in nav bar

--- guardian_app/lib/main.dart
+++ guardian_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import './SplashScreen.dart';
+import './home.dart';
+import './profile.dart';
+import 'MaterialColor.dart';
 
 
 void main() => runApp(MyApp());
@@ -11,7 +14,75 @@ class MyApp extends StatelessWidget {
 
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Montserrat'), 
-      home: SplashScreen());
+      home: MyHomePage());
        
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Widget> pages = [
+    Home(),
+    ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        body: TabBarView(
+          children: pages,
+        ),
+        bottomNavigationBar: Container(
+          // decoration: BoxDecoration(
+          //   boxShadow: [
+          //     BoxShadow(
+          //       offset: Offset(10, 10)
+          //     )
+          //   ]
+          // ),
+          margin: EdgeInsets.only(bottom: 20),
+          child: new TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+                child: Text(
+                  'Home',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat', fontWeight: FontWeight.w600),
+                ),
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+                child: Text(
+                  'Home',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat', fontWeight: FontWeight.w600),
+                ),
+              ),
+            ],
+            unselectedLabelColor: createMaterialColor(Color(0xFFC4C4C4)),
+            labelColor: createMaterialColor(Color(0xFF5ACFE4)),
+            indicatorColor: Colors.transparent,
+          ),
+          
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: new FloatingActionButton(
+          backgroundColor: Colors.redAccent,
+          onPressed:(){ },
+          child: Text("SOS",
+            style: TextStyle(
+                fontFamily: 'Montserrat', fontWeight:FontWeight.bold,color: Colors.white),
+          ),
+        ),
+      ),
+    );
   }
 }
