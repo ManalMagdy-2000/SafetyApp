822c4d0156b6d59f2978a831c492bd5ce455a8b4
manal magdy <47181965+manalmagdy@users.noreply.github.com>
2021-01-28 21:10:24 +0800

    background color changed & text box added

--- guardian_app/lib/CustomMessage.dart
+++ guardian_app/lib/CustomMessage.dart
@@ -1,31 +1,35 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import './home.dart';
+
 class CustomMessagePage extends StatefulWidget {
   @override
-  _CustomMessagePageState createState () => new _CustomMessagePageState();
-
+  _CustomMessagePageState createState() => new _CustomMessagePageState();
 }
 
 class _CustomMessagePageState extends State<CustomMessagePage> {
   int selectedRadio;
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    selectedRadio=0;
+    selectedRadio = 0;
   }
-  setSelectedRadio(int val){
+
+  setSelectedRadio(int val) {
     setState(() {
-      selectedRadio=val;
+      selectedRadio = val;
     });
   }
-  Widget build(BuildContext context){
+
+  Widget build(BuildContext context) {
     return new Scaffold(
       resizeToAvoidBottomInset: true,
-      backgroundColor: Colors.cyan[50],
+      backgroundColor: Colors.white,
       appBar: new AppBar(
         elevation: 0,
         brightness: Brightness.light,
-        backgroundColor: Colors.cyan[50],
+        backgroundColor: Colors.white,
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
@@ -33,74 +37,100 @@ class _CustomMessagePageState extends State<CustomMessagePage> {
           icon: Icon(Icons.arrow_back_ios, size: 20, color: Colors.black),
         ),
       ),
-        body: SingleChildScrollView(
-        child: Container(
+      body: SingleChildScrollView(
+          child: Container(
         padding: EdgeInsets.symmetric(horizontal: 20),
-         width: double.infinity,
-    child: Column(
-    children: <Widget>[
-      Container(
-        child:Align(
-          alignment:Alignment.topLeft
-          ,
-          child:RichText(text: TextSpan(
-              text: 'Customize \n  Message ',
-              style: TextStyle(color: Colors.grey[800],fontSize: 30,fontWeight:FontWeight.bold),
-          ),
-
-          ),),),
-      Padding(
-          padding: const EdgeInsets.only(bottom:50.0)
-      ),
-      RadioListTile(
-        title:Text('Default Message') ,
-        value: 1,
-        groupValue: selectedRadio,
-        activeColor: Colors.amber,
-        onChanged: (newSelectedRadio){
-          setState(() {
-            selectedRadio=newSelectedRadio;
-          });
-        },
-      ),
-      const SizedBox(height: 30),
-      RadioListTile(
-        title: Text('Customize Message'),
-        value: 2,
-        groupValue: selectedRadio,
-        activeColor: Colors.amber,
-        onChanged: (newSelectedRadio){
-          setState(() {
-            selectedRadio=newSelectedRadio;
-          });
-        },
-      ),
-
-
-      MaterialButton(
-        minWidth:280,
-        height: 60,
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(
-              builder: (context) => Home()
-          ));
-        },
-        color: Colors.amber,
-        elevation: 0,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50)
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Customize Emergency\n  Message ',
+                    style: TextStyle(
+                        color: Colors.grey[800],
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            Padding(padding: const EdgeInsets.only(bottom: 30.0)),
+            RadioListTile(
+              title: Text(
+                'Default Message',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.grey[600]),
+              ),
+              value: 1,
+              groupValue: selectedRadio,
+              activeColor: Colors.amber,
+              onChanged: (newSelectedRadio) {
+                setState(() {
+                  selectedRadio = newSelectedRadio;
+                });
+              },
+            ),
+            Padding(padding: const EdgeInsets.only(top: 10.0)),
+            new Container(
+              alignment: Alignment.centerLeft,
+              height: 60.0,
+              width: 320,
+              decoration: new BoxDecoration(color: Colors.grey[200]),
+              child: Text(
+                "\t  Alex is in trouble",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            Padding(padding: const EdgeInsets.only(top: 20.0)),
+            RadioListTile(
+              title: Text('Customize Message',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.grey[600])),
+              value: 2,
+              groupValue: selectedRadio,
+              activeColor: Colors.amber,
+              onChanged: (newSelectedRadio) {
+                setState(() {
+                  selectedRadio = newSelectedRadio;
+                });
+              },
+            ),
+            new Container(
+              alignment: Alignment.center,
+              height: 250.0,
+              width: 320,
+              decoration: new BoxDecoration(color: Colors.grey[200]),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter Message",
+                ),
+              ),
+            ),
+            MaterialButton(
+              minWidth: 280,
+              height: 60,
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Home()));
+              },
+              color: Colors.amber,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              child: Text(
+                "Save",
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                    color: Colors.white),
+              ),
+            ),
+          ],
         ),
-        child: Text("Save", style: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 18,
-            color: Colors.white
-
-        ),),
-      ),
-
-      ],),
-    )),
-
+      )),
     );
   }
 }
